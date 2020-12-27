1f60bf111adf178e4fa99ca1c84cfda7da021993
David Cingolani <cingolanidavid@gmail.com>
2020-12-27 09:52:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="divClass">Hello CodeSandbox</div>
+    </div>
+  );
+}
